110cc20ea66a90b50333ddda0f4d24b00c91e86d
Valentina Castillo Valdés <vpazcastle@gmail.com>
2025-03-29 10:05:24 -0300

    add update and delete products functionality

--- src/functions.py
+++ src/functions.py
@@ -1,9 +1,10 @@
+from CRUD import create_product
 from CRUD import read_products
 from CRUD import read_product
+from CRUD import read_product_by_sku
 from CRUD import read_products_category
-from CRUD import create_product
-
-
+from CRUD import update_product_by_sku
+from CRUD import delete_product
 
 def menu_inicio():
     print("\n--- Inicio de Sesión ---")
@@ -52,15 +53,31 @@ def menu_gestionar_inventario():
 
 
         elif opcion == "2":
-            categoria = input("Ingrese la categoría: ")
-            print(categoria)
+            sku = int(input("\n > Ingrese el SKU del producto a actualizar: "))
+
+            print("\n >> Detalles actuales del producto a actualizar:")
+            producto = read_product_by_sku(sku)
+            if producto:
+                print(f" >>> Nombre: {producto[1]}")
+                print(f" >>> Descripción: {producto[2]}")
+                print(f" >>> Precio: {producto[4]}")
+                print(f" >>> Categoría: {producto[5]}")
+
+            nuevo_nombre = input("\n > Ingrese nuevo nombre (dejar vacío para no cambiar): ")
+            nueva_descripcion = input(" > Ingrese nueva descripción (dejar vacío para no cambiar): ")
+            nuevo_precio = solicitar_numero(" > Ingrese nuevo precio (deja en blanco para mantener el actual): ", tipo=float)
+            nueva_categoria = input(" > Ingrese nueva categoria (dejar vacío para no cambiar): ")
+
+            update_product_by_sku(sku, nuevo_nombre, nueva_descripcion, nueva_categoria, nuevo_precio)
+
         elif opcion == "3":
-            nombre = input("Ingrese el nombre del producto: ")
-            print(nombre)
+            nombre = input("\n > Ingrese el nombre del producto: ")
+            delete_product(nombre)
+
         elif opcion == "4":
             break
         else:
-            print("Opción no válida.")
+            print(" >> Opción no válida.")
 
 def menu_ver_productos():
     while True:
@@ -74,30 +91,31 @@ def menu_ver_productos():
 
         if opcion == "1":
             productos = read_products()
-            print("\nListado de Productos")
+            print("\n >> Listado de Productos: \n")
             print("SUK | Nombre | Descripción | Cantidad | Precio | Categoría")
             for producto in productos:
                 print(f"{producto[0]} | {producto[1]} | {producto[2]} | {producto[3]} | {producto[4]} | {producto[5]}")
         
         elif opcion == "2":
-            categoria = input("Ingrese la categoría: ")
+            categoria = input("\n > Ingrese la categoría: ")
             productos = read_products_category(categoria)
-            print(f"\nListado de Productos {categoria}")
+            print(f"\n >> Listado de Productos {categoria}: \n")
             print("SUK | Nombre | Descripción | Cantidad | Precio")
             for producto in productos:
                 print(f"{producto[0]} | {producto[1]} | {producto[2]} | {producto[3]} | {producto[4]}")
             
         elif opcion == "3":
-            nombre = input("Ingrese el nombre del producto: ")
+            nombre = input("\n > Ingrese el nombre del producto: ")
             producto = read_product(nombre)
             if producto:
-                print(f"Nombre: {producto[1]}")
-                print(f"Descripción: {producto[2]}")
-                print(f"Cantidad: {producto[3]}")
-                print(f"Precio: {producto[4]}")
-                print(f"Categoría: {producto[5]}")
+                print("\n >> Detalles del producto encontrado:")
+                print(f" >> Nombre: {producto[1]}")
+                print(f" >> Descripción: {producto[2]}")
+                print(f" >> Cantidad: {producto[3]}")
+                print(f" >> Precio: {producto[4]}")
+                print(f" >> Categoría: {producto[5]}")
                 
         elif opcion == "4":
             break
         else:
-            print("Opción no válida.")
+            print(" >> Opción no válida.")
